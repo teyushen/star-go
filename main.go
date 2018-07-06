85487e8b13c79cff4d67d61b68ce95678c8c5e17
teyushen <teyushen@gmail.com>
2018-07-06 18:30:34 +0800

    test

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+		"net/http"
+		"os"
+	    "io/ioutil"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	resp, err := http.Get("http://example.com/")
+    if err != nil {
+        fmt.Printf("%s", err)
+        os.Exit(1)
+    } else {
+        defer resp.Body.Close()
+        contents, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+            fmt.Printf("%s", err)
+            os.Exit(1)
+        }
+        fmt.Printf("%s\n", string(contents))
+    }
 }
